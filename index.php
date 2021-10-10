1509cd638aef68ea7a1d41fce65820c85f24a65d
Roland <csehi.rolcsi@gmail.com>
2021-10-10 12:00:22 +0200

    ehe

--- index.php
+++ index.php
@@ -61,6 +61,30 @@
             </ul>
         </div>
     </nav>
+    <?php
+$queryString = http_build_query([
+    'access_key' => 'secrets.API_KEY',
+    'arr_iata' => 'BUD'
+  ]);
+  
+  $ch = curl_init(sprintf('%s?%s', 'http://api.aviationstack.com/v1/flights', $queryString));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  
+  $json = curl_exec($ch);
+  curl_close($ch);
+  
+  $api_result = json_decode($json, true);
+  foreach ($api_result['data'] as $flight) {
+          echo sprintf("%s flight %s from %s (%s) to %s (%s) is in the air.<br>",
+              $flight['airline']['name'],
+              $flight['flight']['iata'],
+              $flight['departure']['airport'],
+              $flight['departure']['iata'],
+              $flight['arrival']['airport'],
+              $flight['arrival']['iata']
+              ), PHP_EOL;
+  }
+?>
     <script>
         let btn = document.querySelector("#btn");
         let sidebar = document.querySelector(".sidebar");

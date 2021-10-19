f0462d3a250253c98136ac30e77046769fe4c81d
Tpeti <petike804@gmail.com>
2021-10-19 10:55:06 +0200

    abra

---
+++ about.php
@@ -0,0 +1,36 @@
+<?php
+
+$title = "Rólunk";
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title><?= $title ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+
+<h1>Üdvözlünk az oldalon</h1>
+
+<p>
+Mi vagyunk az ASTP. Repüljünk együtt!
+
+
+
+</p>
+
+
+</body>
+</html>
---
+++ src/app/model/Repulo.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace app\model;
+
+class Repulo{
+    
+private $erkezik;
+private $indul;
+
+
+
+public function getErkezett() {
+
+return $this->erkezik;
+
+}
+
+public function getIndul() {
+
+    return $this->indul;
+    
+    }
+
+    
+
+
+}
+
+?>

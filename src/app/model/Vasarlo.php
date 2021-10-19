39c6a43adbe8ddab623c98cb6240bc06507b5902
Kiss Ábrahám <abris.kiss96@freemail.hu>
2021-10-19 11:54:18 +0200

    abra

---
+++ src/app/model/Vasarlo.php
@@ -0,0 +1,32 @@
+<?php
+
+class Vasarlo {
+private $id;
+private $nev;
+private $repulo;
+
+public function  getId() {
+   
+  return $this->id;  
+}
+
+
+
+public function  getNev() {
+   
+    return $this->nev;  
+  }
+
+  
+public function  getRepulo() {
+   
+    return $this->repulo;  
+  }
+
+
+
+}
+
+
+
+?>

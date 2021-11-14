ace55fd1bd687c218ca1229976115ca7cc3cca59
Roland <csehi.rolcsi@gmail.com>
2021-11-14 17:41:41 +0100

    tabolásokat rendeztem

--- src/app/model/Repulo.php
+++ src/app/model/Repulo.php
@@ -1,67 +1,41 @@
 <?php
 
-
 namespace app\model;
 
-class Repulo{
-   
-private $id;
-private $tipus;
-private $ut;    
-private $erkezik;
-private $indul;
-
-
- public function con($id , $tipus , $ut , $erkezik , $indul) {
-
-    $this->id = $id; 
-    $this->tipus = $tipus; 
-    $this->ut = $ut; 
-    $this->erkezik = $erkezik;  
-$this->indul = $indul; 
-
-
-}
-
-
-
-public function getId()
+class Repulo
 {
+    private $id;
+    private $tipus;
+    private $ut;
+    private $erkezik;
+    private $indul;
 
-return $this->id;
-
-}
-
-public function getTipus() {
-
-   return $this->tipus; 
-}
-
-public function getUt() {
-
-    return $this->ut; 
- }
-
-
-public function getErkezett() {
-
-return $this->erkezik;
-
-}
-
-public function getIndul() {
-
-    return $this->indul;
-    
+    public function con($id , $tipus , $ut , $erkezik , $indul) {
+        $this->id = $id;
+        $this->tipus = $tipus;
+        $this->ut = $ut;
+        $this->erkezik = $erkezik;
+        $this->indul = $indul;
     }
 
+    public function getId() {
+        return $this->id;
+    }
 
+    public function getTipus() {
+        return $this->tipus;
+    }
 
+    public function getUt() {
+        return $this->ut;
+    }
 
+    public function getErkezett() {
+        return $this->erkezik;
+    }
+
+    public function getIndul() {
+        return $this->indul;
+    }
 }
-
-
-
-
-
 ?>

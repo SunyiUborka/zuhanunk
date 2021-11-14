003b271e6970a4fa854d8c7525f962e98f8020ea
Roland <csehi.rolcsi@gmail.com>
2021-11-14 17:42:12 +0100

    iconokat cseréltem

--- airports.php
+++ airports.php
@@ -26,55 +26,41 @@ $queryString = http_build_query([
 <?php
 include_once("nav.php");
 ?>
-<div class="container-fluid">
-    <div class="row">
-    <table class="table">
-    <thead>
-        <tr>
-            <th>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cursor" viewBox="0 0 16 16">
-  <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z"/>
-</svg>
-
-                Repülőtér
-
-            </th>
-            <th>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-</svg>
-
-                Ország
-            </th>
-            <th>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
-  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
-  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
-</svg>
- Időzóna
-            </th>
-            <th>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
-  <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"/>
-</svg>
-
-                GMT
-            </th>
-        </tr>
-    </thead>
-    <?php foreach ($api_result['data'] as $flight):?>
-    <tbody>
-        <tr>
-            <td><?=$flight["airport_name"]?></td>
-            <td><?=$flight["country_name"]?></td>
-            <td><?=$flight["timezone"]?></td>
-            <td><?=$flight["gmt"]?></td>
-        </tr>     
-    </tbody>
-    <?php endforeach ?>
-</table>
+    <div class="container-fluid">
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>
+                            <i class='bx bxs-plane-take-off' ></i>
+                            Repülőtér
+                        </th>
+                        <th>
+                            <i class='bx bx-map-pin' ></i>
+                            Ország
+                        </th>
+                        <th>
+                            <i class='bx bx-time-five'></i>
+                            Időzóna
+                        </th>
+                        <th>
+                            <i class='bx bx-timer'></i>
+                            GMT
+                        </th>
+                    </tr>
+                </thead>
+                <?php foreach ($api_result['data'] as $flight):?>
+                    <tbody>
+                        <tr>
+                            <td><?=$flight["airport_name"]?></td>
+                            <td><?=$flight["country_name"]?></td>
+                            <td><?=$flight["timezone"]?></td>
+                            <td><?=$flight["gmt"]?></td>
+                        </tr>
+                    </tbody>
+                <?php endforeach ?>
+            </table>
+        </div>
     </div>
-</div>
 </body>
 </html>

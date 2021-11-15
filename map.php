a91480f09f027fac4750579f558a3a6a085e2d39
Roland <csehi.rolcsi@gmail.com>
2021-11-15 23:12:02 +0100

    térkép

---
+++ map.php
@@ -0,0 +1,28 @@
+<?php
+include_once("config.php");
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <link rel="icon" href="img/plane.png"/>
+    <link rel="shortcut icon" href="img/plane.png"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zuhanunk</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<?php
+include_once("nav.php");
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+
+        </div>
+    </div>
+</div>
+</body>
+</html>

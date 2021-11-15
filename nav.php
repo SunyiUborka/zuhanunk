f0aaa4c2080945daee2e014d1b054aca4be63c35
Roland <csehi.rolcsi@gmail.com>
2021-11-15 23:12:31 +0100

    tékép hozzáadva

--- nav.php
+++ nav.php
@@ -2,10 +2,10 @@
 <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <nav>
-        <div class="sidebar">
-            <div class="logo-details">
-                <i class='bx bx-radar icon' ></i>
-                    <div class="logo_name">BRUH</div>  
+    <div class="sidebar">
+        <div class="logo-details">
+            <i class='bx bx-radar icon' ></i>
+            <div class="logo_name">BRUH</div>
                 <i class='bx bx-menu-alt-left' id="btn"></i>
             </div>
             <ul class="nav_list">
@@ -22,7 +22,7 @@
                     <span class="tooltip">Keresés</span>
                 </li>
                 <li>
-                    <a href="">
+                    <a href="map.php">
                         <i class='bx bx-map-alt' ></i>
                         <span class="links_name">Térkép</span>
                     </a>
@@ -56,7 +56,6 @@
         let btn = document.querySelector("#btn");
         let sidebar = document.querySelector(".sidebar");
         let searchBtn = document.querySelector(".bx-search");
-
         btn.onclick = function(){
             sidebar.classList.toggle("active");
         }

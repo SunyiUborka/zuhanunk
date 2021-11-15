d7b87b3b658b01b2c8e4525046641a188cd7933c
Roland <csehi.rolcsi@gmail.com>
2021-11-15 23:01:48 +0100

    weboldal icont hozzáadtam

--- info.php
+++ info.php
@@ -13,10 +13,12 @@ $beiras = $_POST['email'] ?? null;
 <!DOCTYPE html>
 <html lang="hu">
 <head>
+    <link rel="icon" href="img/plane.png"/>
+    <link rel="shortcut icon" href="img/plane.png"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
- <title><?= $title ?></title>
+    <title><?= $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="./style.css">
